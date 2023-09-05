7174d0c9f3a52b6aa200225cd334e2fd4caa11ca
sergeykurilov <kurilovsergey15@gmail.com>
2023-09-05 08:54:48 +0300

    feat: adding logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import CounterAppTwo from "./components/CounterAppTwo";
 
 const App = () => (
-	<div >
-		<div>APP-2</div>
-		<div>
-			<CounterAppTwo />
-		</div>
-	</div>
+  <div>
+    <CounterAppTwo />
+  </div>
 );
 
 export default App;
--- src/components/CounterAppTwo.tsx
+++ src/components/CounterAppTwo.tsx
@@ -5,7 +5,7 @@ const Counter = () => {
   const [count, setCount] = useState(1);
 
   return (
-    <div className="flex h-full items-center justify-center bg-[#47d726] text-center font-[bold] text-lg">
+    <div className="flex h-full items-center justify-center bg-[#FCF3DBFF] text-center font-[bold] text-lg">
       <div className="flex flex-col">
         <div>Your click count : {count} </div>
         <button onClick={() => setCount((prevState) => prevState * 2)}>
