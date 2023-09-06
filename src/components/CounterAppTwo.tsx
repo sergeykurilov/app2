24dd64689c5204452042b6d578fd9c443000d8fc
sergeykurilov <kurilovsergey15@gmail.com>
2023-09-06 03:50:32 +0300

    feat: adding logo

--- src/components/CounterAppTwo.tsx
+++ src/components/CounterAppTwo.tsx
@@ -6,25 +6,16 @@ const Counter = () => {
   const [count, setCount] = useState(1);
 
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const handleEvent = (e) => {
+    const handleEvent = (e: any) => {
       if (e.detail.from === "APP1") {
         setCount(e.detail.count * 2);
       }
     };
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const handleBroadcast = (e) => {
-      alert(`Broadcast received in APP2: ${e.detail.message}`);
-    };
 
     window.addEventListener("counterChange", handleEvent);
-    window.addEventListener("broadcast", handleBroadcast);
 
     return () => {
       window.removeEventListener("counterChange", handleEvent);
-      window.removeEventListener("broadcast", handleBroadcast);
     };
   }, []);
 
@@ -36,12 +27,7 @@ const Counter = () => {
       detail: { from: "APP2", count: newCount },
     });
 
-    const broadcastEvent = new CustomEvent("broadcast", {
-      detail: { message: "Hello from APP2" },
-    });
-
     window.dispatchEvent(event);
-    window.dispatchEvent(broadcastEvent);
   };
   return (
     <div className="flex h-full items-center justify-center bg-[#FCF3DBFF] text-center font-[bold] text-lg">
